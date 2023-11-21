361546d64882138656e406decae939d5edf21df6
Oliwier <olikucz@vp.pl>
2023-11-21 02:51:52 +0100

    finished

--- index.php
+++ index.php
@@ -58,58 +58,47 @@
             
             
             if(isset($_GET["search-string"]) && (empty($_GET["search-string"]) === false)) {
-              $sql .= " WHERE `name` LIKE '%?%' OR `email` LIKE '%?%' OR `message` LIKE '%?%' OR `date` LIKE '%?%' OR `status` LIKE '%?%' ";
+              $sql .= " WHERE `name` LIKE '%{$_GET["search-string"]}%' OR `email` LIKE '%{$_GET["search-string"]}%' OR `message` LIKE '%{$_GET["search-string"]}%' OR `date` LIKE '%{$_GET["search-string"]}%' OR `status` LIKE '%{$_GET["search-string"]}%' ";
             }
 
             if(isset($_GET["number-of-records"]) && empty($_GET["number-of-records"]) === false && $_GET["number-of-records"] !== "all") {
               $sql .= " LIMIT {$_GET["number-of-records"]} ";
             }
 
-            $stmt = $conn->prepare($sql);
+            $result = $conn->query($sql);
             
-            if(isset($_GET["search-string"])) {
-              $stmt->bind_param("sssss", $_GET["search-string"], $_GET["search-string"], $_GET["search-string"], $_GET["search-string"], $_GET["search-string"]);
-            }
-
-            if(isset($_GET["number-of-records"]) && $_GET["number-of-records"] !== "all") {
-              $stmt->bind_param("i", $_GET["number-of-records"]);
-            }
-
-            $stmt->execute();
-            $result = $stmt->get_result();
-
             if ($result->num_rows > 0) {
               while ($row = $result->fetch_assoc()) {
-                  echo <<<END
-                      <tr data-id="{$row["id"]}" data-name="{$row["name"]}" data-email="{$row["email"]}" data-message="{$row["message"]}">
-                          <td>{$row["name"]}</td>
-                          <td>{$row["email"]}</td>
-                          <td>{$row["date"]}</td>
-                          <td class='txt-a-c'>
-                              <span class='indicator'>{$row["status"]}</span>
-                          </td>
-                          <td class='txt-a-c'>
-                              <button class='btn' onclick="showMessage('{$row["message"]}')">Wiadomość</button>
-                          </td>
-                          <td class='flex jc-c gap'>
-                              <button class='action-btn' onclick="openEdit(this)">
-                                  <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                                      <path d='M5 19H6.425L16.2 9.225L14.775 7.8L5 17.575V19ZM3 21V16.75L16.2 3.575C16.4 3.39167 16.6208 3.25 16.8625 3.15C17.1042 3.05 17.3583 3 17.625 3C17.8917 3 18.15 3.05 18.4 3.15C18.65 3.25 18.8667 3.4 19.05 3.6L20.425 5C20.625 5.18333 20.7708 5.4 20.8625 5.65C20.9542 5.9 21 6.15 21 6.4C21 6.66667 20.9542 6.92083 20.8625 7.1625C20.7708 7.40417 20.625 7.625 20.425 7.825L7.25 21H3ZM15.475 8.525L14.775 7.8L16.2 9.225L15.475 8.525Z'/>
-                                  </svg>
-                              </button>
-                              <button class='action-btn' onclick="deletePost({$row['id']})">
-                                  <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                                      <path d='M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6H4V4H9V3H15V4H20V6H19V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM17 6H7V19H17V6ZM9 17H11V8H9V17ZM13 17H15V8H13V17Z'/>
-                                  </svg>
-                              </button>
-                          </td>
-                      </tr>
-              END;
-                  }
+                echo <<<END
+                <tr data-id="{$row["id"]}" data-name="{$row["name"]}" data-email="{$row["email"]}" data-message="{$row["message"]}">
+                <td>{$row["name"]}</td>
+                <td>{$row["email"]}</td>
+                <td>{$row["date"]}</td>
+                <td class='txt-a-c'>
+                <span class='indicator'>{$row["status"]}</span>
+                </td>
+                <td class='txt-a-c'>
+                <button class='btn' onclick="showMessage('{$row["message"]}')">Wiadomość</button>
+                </td>
+                <td class='flex jc-c gap'>
+                <button class='action-btn' onclick="openEdit(this)">
+                <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M5 19H6.425L16.2 9.225L14.775 7.8L5 17.575V19ZM3 21V16.75L16.2 3.575C16.4 3.39167 16.6208 3.25 16.8625 3.15C17.1042 3.05 17.3583 3 17.625 3C17.8917 3 18.15 3.05 18.4 3.15C18.65 3.25 18.8667 3.4 19.05 3.6L20.425 5C20.625 5.18333 20.7708 5.4 20.8625 5.65C20.9542 5.9 21 6.15 21 6.4C21 6.66667 20.9542 6.92083 20.8625 7.1625C20.7708 7.40417 20.625 7.625 20.425 7.825L7.25 21H3ZM15.475 8.525L14.775 7.8L16.2 9.225L15.475 8.525Z'/>
+                </svg>
+                </button>
+                <button class='action-btn' onclick="deletePost({$row['id']})">
+                <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6H4V4H9V3H15V4H20V6H19V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM17 6H7V19H17V6ZM9 17H11V8H9V17ZM13 17H15V8H13V17Z'/>
+                </svg>
+                </button>
+                </td>
+                </tr>
+                END;
               }
-
-              $stmt->close();
-              $conn->close();
+            }
+            
+            mysqli_free_result($result);
+            $conn->close();
           ?>
         </tbody>
       </table>
